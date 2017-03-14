151fed9051f2fc863ed485ab1026531ea38df145
Matt Sobieray <matts@thefirmgraphics.com>
2017-03-14 17:57:19 -0600

    added vue-install and docker-compose.yml

---
+++ src/components/Hello.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <nav>
+      <ul>
+        <li><router-link to="/posts">Go to Posts</router-link></li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Posts.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="posts">
+		<h1>{{ posts }}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      posts: 'Welcome to Your Posts'
+    }
+  }
+}
+</script>
